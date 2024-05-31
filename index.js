8c4c96c29b2174d17abd1bd385f1e36cf64c0958
mukul chauhan <mukulchauhan7407@gmail.com>
2024-05-31 15:05:48 +0530

    helo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-let port=8080;
+let port= process.env.PORT|| 8080;
 const path =require("path");
 const databaseconnection=require("./databaseconnection");
 const userregistration=require("./models/userregistration");
